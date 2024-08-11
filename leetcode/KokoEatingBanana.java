50b7db2cc7e02ce749189d39527b0120660e5fbd
Rishabh Agarwal <rishabhagarwal@Rishabhs-MacBook-Air.local>
2024-08-11 02:26:52 -0500

    auto commit on 2024-08-11 02:26:52

---
+++ leetcode/KokoEatingBanana.java
@@ -0,0 +1,25 @@
+package leetcode;
+
+public class KokoEatingBanana {
+    public static int minEatingSpeed(int[] piles, int h) {
+        int speed = 1;
+
+        while (true) {
+            int hourspent = 0;
+            for (int pile: piles) {
+                hourspent += Math.ceil((double)pile / speed);
+                if (hourspent > h)
+                    break;
+            }
+            if(hourspent<=h)
+                return speed;
+            else
+                speed++;
+        }
+    }
+
+    public static void main(String[] args) {
+        //System.out.println(minEatingSpeed(new int[]{3,6,7,11}, 8));
+        System.out.println(minEatingSpeed(new int[]{30,11,23,4,20}, 5));
+    }
+}
