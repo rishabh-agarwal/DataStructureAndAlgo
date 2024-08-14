ab5ece8c5f2921fdea84bc877b3eafb21c7e003e
Rishabh Agarwal <rishabhagarwal@Rishabhs-MacBook-Air.local>
2024-08-13 20:06:05 -0500

    auto commit on 2024-08-13 20:06:05

--- leetcode/ThreeSum.java
+++ leetcode/ThreeSum.java
@@ -14,12 +14,21 @@ public class ThreeSum {
         int n = nums.length;
 
         for(int i=0; i<n-2; i++){
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
             int left = i+i;
             int right = n-1;
             while(left<right){
                 int sum = nums[i]+nums[left]+nums[right];
                 if(sum == 0){
                     result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    while (left < right && nums[left] == nums[left + 1]) {
+                        left++;
+                    }
+                    while (left < right && nums[right] == nums[right - 1]) {
+                        right--;
+                    }
                     left++;
                     right--;
                 }else if(sum<0){
