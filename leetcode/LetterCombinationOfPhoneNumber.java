65a083886fe3053f846f578bf4551625b5d0b653
Rishabh Agarwal <rishabhagarwal@Rishabhs-MacBook-Air.local>
2024-08-15 23:00:49 -0500

    auto commit on 2024-08-15 23:00:49

--- leetcode/LetterCombinationOfPhoneNumber.java
+++ leetcode/LetterCombinationOfPhoneNumber.java
@@ -9,36 +9,22 @@ import java.util.List;
 public class LetterCombinationOfPhoneNumber {
 
     public static List<String> letterCombinations(String digits) {
-        String[] KEYPAD = {
-                "",     // 0
-                "",     // 1
-                "abc",  // 2
-                "def",  // 3
-                "ghi",  // 4
-                "jkl",  // 5
-                "mno",  // 6
-                "pqrs", // 7
-                "tuv",  // 8
-                "wxyz"  // 9
-        };
+        if(digits == null || digits.isEmpty()) return new ArrayList<>();
+       String[] keypad = {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+       List<String> result = new ArrayList<>();
+       result.add("");
 
-        List<String> result = new ArrayList<>();
-        if(digits == null || digits.isEmpty()){
-            return result;
-        }
-
-        result.add("");
-        for(char digit: digits.toCharArray()){
-            String letters = KEYPAD[digit - '0'];
-            List<String> newCombinations = new ArrayList<>();
-            for(String combination: result){
-                for (char letter : letters.toCharArray()) {
-                    newCombinations.add(combination + letter);
-                }
-            }
-            result = newCombinations;
-        }
-        return result;
+       for(char c: digits.toCharArray()){ //Iterating 1 digit over time eg: 3 time (2 -> abc)
+           char[] letters = keypad[c - '0'].toCharArray(); //eg: abc, def
+           List<String> newCombination = new ArrayList<>();
+           for(String res: result){
+               for(char letter : letters){ //3 time
+                   newCombination.add(res + letter);
+               }
+           }
+           result = newCombination;
+       }
+       return result;
     }
     public static void main(String[] args) {
         System.out.println(letterCombinations("234")); // Output: ["ad","ae","af","bd","be","bf","cd","ce","cf"]
